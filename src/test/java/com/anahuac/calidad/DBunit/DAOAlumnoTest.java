da2b2e34411bcf3ccfc21439c7c77c929ee0301e
Fernanda Pacheco Hdz <67036275+RefCap@users.noreply.github.com>
2022-05-14 09:24:02 -0500

    imports
    import static org.hamcrest.Matchers.*;
    import static org.junit.Assert.assertThat;

--- src/test/java/com/anahuac/calidad/DBunit/DAOAlumnoTest.java
+++ src/test/java/com/anahuac/calidad/DBunit/DAOAlumnoTest.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 import org.dbunit.Assertion;
 import org.dbunit.DBTestCase;
